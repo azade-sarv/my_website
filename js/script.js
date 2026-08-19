a8ec805afbda2038114eab6ecf0a0563a3b24ee6
sarv <immortalcypress@gmail.com>
2026-08-19 08:03:41 -0700

    curren page icon effect added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-﻿
+﻿const currentPage = window.location.pathname.split('/').pop();
+
+const navLinks = document.querySelectorAll('.nav-icon');
+
+navLinks.forEach(function(link) {
+    const linkPage = link.getAttribute('href');
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
+});
