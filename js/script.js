7406cd354a3cf094b025b66db94541b2e9fe4e3c
sarv <immortalcypress@gmail.com>
2026-08-19 08:34:28 -0700

    h1 effect added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ navLinks.forEach(function(link) {
         link.classList.add('active');
     }
 });
+
+const title = document.querySelector("h1");
+const text = title.textContent;
+
+title.textContent = "";
+
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        title.textContent += text[i];
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+typeWriter();
